4ca86e08bfeac12bcfa9b5810300b75e6de20d23
student <rufatf@gmail.com>
2015-12-15 20:59:01 +0300

    practice exersice

---
+++ practice_break.py
@@ -0,0 +1,11 @@
+l = [36, 56 , 67 , 658, 412, 42, 555, 668, 22, 95]
+
+for n in l:
+    #print str(n)
+    s = str(n)
+    if len(s)<3:
+        print s
+    if n == 42:
+        print ('The Answer to the Ultimate Question of Life, the Universe, and Everything.')
+        break
+#
